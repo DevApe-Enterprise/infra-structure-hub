746a2c5415b39d32975d3b8af0d58d44609f9bca
Melquias Ribeiro <melquias10.mr@gmail.com>
2026-03-02 10:27:38 -0300

    feat: config naigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Concrete from "./pages/Concrete";
-import Asphalt from "./pages/Asphalt";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CalculatorPage from "./pages/CalculatorPage";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -18,12 +14,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/concrete" element={<Concrete />} />
-          <Route path="/asphalt" element={<Asphalt />} />
           <Route path="/calculadora" element={<CalculatorPage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/calculadora" replace />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
